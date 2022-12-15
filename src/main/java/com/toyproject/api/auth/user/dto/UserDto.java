dbf4589e45640e157e0674012ce518d369b18c23
KimDongHyeon <dhkim@bflysoft.com>
2022-12-15 15:20:46 +0900

    FEAT : 로그인 dto 생성

--- src/main/java/com/toyproject/api/auth/user/dto/UserDto.java
+++ src/main/java/com/toyproject/api/auth/user/dto/UserDto.java
@@ -16,4 +16,17 @@ public class UserDto {
         private int locationYn;
         private String userType;
     }
+
+    @Data
+    public static class signIn{
+        private int uid;
+        private String userId;
+        private String pwd;
+        private String nickname;
+        private int gender;
+        private String birthday;
+        private String accessTp;
+        private int locationYn;
+        private String userType;
+    }
 }
