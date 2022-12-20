19cb1589be3bc28df2e01fb964d60f95059f4284
KimDongHyeon <dhkim@bflysoft.com>
2022-12-20 15:24:51 +0900

    FEAT : 쿠키를 사용한 로그인, 로그아웃 API 구현 및 메소드를 GET -> POST 변경

--- src/main/java/com/toyproject/api/auth/user/controller/UserController.java
+++ src/main/java/com/toyproject/api/auth/user/controller/UserController.java
@@ -9,10 +9,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -38,6 +43,7 @@ public class UserController {
 
         } catch (DuplicateKeyException e) {
             return DefaultResponse.from(BAD_REQUEST.value(), "이메일 또는 닉네임 중복", user).build();
+
         }
         return DefaultResponse.from(CREATED.value(),"아이디 생성 성공", user).build();
     }
@@ -49,9 +55,10 @@ public class UserController {
 //
 //    }
 
+
     @ApiOperation("로그인")
-    @GetMapping("/sign-in")
-    public ResponseEntity<Object> signIn (@RequestBody UserDto.signIn user) {
+    @PostMapping("/sign-in")
+    public ResponseEntity<Object> signIn (@RequestBody UserDto.signIn user, HttpServletResponse response) {
         //입력한 비밀번호 암호화
         String encryPwd = DigestUtils.sha256Hex(user.getPwd());
         user.setPwd(encryPwd);
@@ -59,8 +66,29 @@ public class UserController {
         UserDto.signIn userInfo = userService.getUser(user);
         if(userInfo == null) {
             return DefaultResponse.from(NOT_FOUND.value(), "아이디 또는 비밀번호가 맞지않습니다.", user).build();
-
         }
-        return DefaultResponse.from(NOT_FOUND.value(), "로그인 성공", userInfo).build();
+
+        Cookie idCookie = new Cookie("uid", String.valueOf(userInfo.getUid()));
+
+        response.addCookie(idCookie);
+
+        return DefaultResponse.from(OK.value(), "로그인 성공", userInfo).build();
+    }
+
+    @ApiOperation("로그아웃")
+    @PostMapping("/sign-out")
+    public ResponseEntity<Object> signOut (@CookieValue Cookie uid, HttpServletResponse response) {
+        if(uid == null){
+            return DefaultResponse.from(NOT_FOUND.value(), "옳바르지 않은 요청입니다.", uid).build();
+        }
+
+        expireCookie(response, uid);
+
+        return DefaultResponse.from(OK.value(), "로그아웃 성공", uid).build();
+    }
+
+    private void expireCookie(HttpServletResponse response, Cookie uid) {
+        uid.setMaxAge(0);
+        response.addCookie(uid);
     }
 }
