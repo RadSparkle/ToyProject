e142592b4f88db48d7688a800ce8f10432852fc2
KimDongHyeon <dhkim@bflysoft.com>
2022-12-23 14:10:59 +0900

    FEAT : JWT 토큰을 확인하는 클래스 생성

---
+++ src/main/java/com/toyproject/api/auth/user/jwt/AuthorizationToken.java
@@ -0,0 +1,28 @@
+package com.toyproject.api.auth.user.jwt;
+
+import com.toyproject.api.common.exception.ApiException;
+import com.toyproject.api.common.exception.EnumException;
+import lombok.EqualsAndHashCode;
+
+import java.util.Optional;
+
+import static com.toyproject.api.common.exception.EnumException.TOKEN_IS_WRONG;
+
+@EqualsAndHashCode
+public class AuthorizationToken {
+    private final String authorization;
+    public static final String HEADER_AUTH_KEY = "Authorization";
+
+    private AuthorizationToken(String authorization) {
+        this.authorization = authorization;
+    }
+
+    public static AuthorizationToken of(String authorization) {
+        Optional<String> stringOptional = Optional.ofNullable(authorization);
+
+        if (stringOptional.isPresent()) {
+            return new AuthorizationToken(stringOptional.get());
+        }
+        throw new ApiException(TOKEN_IS_WRONG);
+    }
+}
