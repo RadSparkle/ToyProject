87e41d5f1c93df2434fdf1676c13c19c60d1b3c4
KimDongHyeon <dhkim@bflysoft.com>
2022-12-23 14:56:24 +0900

    FEAT : 생성장에 토큰이비어있을경우에대한 예외처리 추가

--- src/main/java/com/toyproject/api/auth/user/jwt/AuthorizationToken.java
+++ src/main/java/com/toyproject/api/auth/user/jwt/AuthorizationToken.java
@@ -1,5 +1,6 @@
 package com.toyproject.api.auth.user.jwt;
 
+import com.toyproject.api.common.DefaultResponse;
 import com.toyproject.api.common.exception.ApiException;
 import com.toyproject.api.common.exception.EnumException;
 import lombok.EqualsAndHashCode;
@@ -11,9 +12,13 @@ import static com.toyproject.api.common.exception.EnumException.TOKEN_IS_WRONG;
 @EqualsAndHashCode
 public class AuthorizationToken {
     private final String authorization;
+
     public static final String HEADER_AUTH_KEY = "Authorization";
 
     private AuthorizationToken(String authorization) {
+        if (authorization.isBlank()) {
+            throw new ApiException(EnumException.TOKEN_IS_WRONG);
+        }
         this.authorization = authorization;
     }
 
@@ -25,4 +30,9 @@ public class AuthorizationToken {
         }
         throw new ApiException(TOKEN_IS_WRONG);
     }
+
+//    public static AuthorizationToken verify(String authorization) {
+//
+//    }
+
 }
