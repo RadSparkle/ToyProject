fa3b4b45eddff6f1105719e8f7a2218d45dec89c
RadSparkle <wwwartg1@naver.com>
2023-03-09 01:42:37 +0900

    Feat : Token값을 담을 vo 생성

---
+++ src/main/java/com/toyproject/api/common/model/auth/TokenVo.java
@@ -0,0 +1,25 @@
+package com.toyproject.api.common.model.auth;
+
+import com.toyproject.api.common.model.ResponseObject;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class TokenVo extends ResponseObject {
+
+    @ApiModelProperty(value="액세스 토큰")
+    private String access_token;
+
+    @ApiModelProperty(value="액세스 토큰 만료시간(초)")
+    private Integer access_token_expries_in;
+
+    @ApiModelProperty(value="리프레시 토큰")
+    private String refresh_token;
+
+    @ApiModelProperty(value="리프레시 토큰 만료시간(초)")
+    private Integer refresh_token_expires_in;
+
+    @ApiModelProperty(value="토큰유형", example="bearer")
+    private String token_tp = "bearer";
+
+}
