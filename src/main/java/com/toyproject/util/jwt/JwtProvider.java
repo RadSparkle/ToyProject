54ae17be38bd09c98ec98802979f563c07f6406d
RadSparkle <wwwartg1@naver.com>
2023-03-09 17:34:16 +0900

    Feat : Jwt토큰발급, 리프레시토큰 발급등 여러역할을하는 클래스 생성. 현재는 토큰만드는 메소드 구현함

---
+++ src/main/java/com/toyproject/util/jwt/JwtProvider.java
@@ -0,0 +1,51 @@
+package com.toyproject.util.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.*;
+import static com.toyproject.util.jwt.JwtOption.*;
+
+public class JwtProvider {
+
+    private static final String TYPE = "typ";
+
+    private static final String ALGORITHM = "alg";
+
+    private static final String EXPIRED = "exp";
+
+    private static final String ACCESS_TOKEN = "access_token";
+
+    private static final String ACCESS_TYPE = "access_tp";
+
+    private static final String UID = "uid";
+
+    private static final String USER_ID = "user_id";
+
+    private static final String USER_TYPE = "user_tp";
+
+    public String createAccessToken(JwtPayLoad payLoad, String key) {
+        return createAccessToken(payLoad, key, null);
+    }
+
+    public String createAccessToken(JwtPayLoad payLoad, String key, Long addExpiredTime) {
+        Map<String, Object> headers = new HashMap<>();
+        headers.put(TYPE, JWT_TYP); // 타입 : JWT
+        headers.put(ALGORITHM, JWT_ALG); // 알고리즘방식 : HS256
+
+        Date date = new Date();
+        long exp = date.getTime() + JWT_ACCESS_EXPIRED_TIME;
+        if (addExpiredTime != null) {
+            exp = date.getTime() + addExpiredTime;
+        }
+
+        payLoad.setExp(exp);
+
+        return Jwts.builder()
+                .setExpiration(new Date(exp))
+                .setHeader(headers)
+                .setClaims(payLoad.getMap())
+                .signWith(SignatureAlgorithm.HS256, key.getBytes())
+                .compact();
+    }
+}
