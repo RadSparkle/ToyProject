f55a83a03ad8fe2a01164c76ac4170031ef26c21
RadSparkle <wwwartg1@naver.com>
2023-03-09 17:32:04 +0900

    Refactor : 줄개행

--- src/main/java/com/toyproject/util/jwt/JwtOption.java
+++ src/main/java/com/toyproject/util/jwt/JwtOption.java
@@ -11,8 +11,11 @@ public class JwtOption {
     public static final String JWT_ALG = "HS256"; //JWT 알고리즘 방식 : HS256
 
     public static final String JWT_AUTH_TYPE = "Bearer";
+
     public static final String JWT_SECRET_KEY = "toyproject:root";
+
     public static final long JWT_ACCESS_EXPIRED_TIME = 1000*60*60*3L;
+
     public static final long JWT_REFRESH_EXPIRED_TIME = 1000*60*60*24*14L;
 
     private JwtOption(){}
