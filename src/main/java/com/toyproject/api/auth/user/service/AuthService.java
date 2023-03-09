16184a3264d97c909645ee6addf4637b9ce914a2
RadSparkle <wwwartg1@naver.com>
2023-03-09 17:33:25 +0900

    Feat : getToken 메소드 추가

--- src/main/java/com/toyproject/api/auth/user/service/AuthService.java
+++ src/main/java/com/toyproject/api/auth/user/service/AuthService.java
@@ -1,9 +1,13 @@
 package com.toyproject.api.auth.user.service;
 
 import com.toyproject.api.auth.user.dto.AuthDto;
+import com.toyproject.api.common.model.auth.TokenVo;
+import com.toyproject.util.jwt.JwtPayLoad;
 
 public interface AuthService {
     void insertUser(AuthDto.signUp user);
 
     AuthDto.signIn getUser(AuthDto.signIn user);
+
+    TokenVo getToken(JwtPayLoad build);
 }
--- src/main/java/com/toyproject/api/auth/user/service/impl/AuthServiceImpl.java
+++ src/main/java/com/toyproject/api/auth/user/service/impl/AuthServiceImpl.java
@@ -3,6 +3,10 @@ package com.toyproject.api.auth.user.service.impl;
 import com.toyproject.api.auth.user.dto.AuthDto;
 import com.toyproject.api.auth.user.mapper.AuthMapper;
 import com.toyproject.api.auth.user.service.AuthService;
+import com.toyproject.api.common.model.auth.TokenVo;
+import com.toyproject.util.jwt.JwtOption;
+import com.toyproject.util.jwt.JwtPayLoad;
+import com.toyproject.util.jwt.JwtProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +25,13 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public AuthDto.signIn getUser(AuthDto.signIn user) {
         return authMapper.getUser(user);
+    }
+
+    @Override
+    public TokenVo getToken(JwtPayLoad build) {
+        String key = JwtOption.encodeSecretKey();
+        JwtProvider provider = new JwtProvider();
+
+        return null;
     }
 }
